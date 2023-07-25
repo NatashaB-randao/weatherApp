4885fccc4452cca618ffd6da1c2eef06ffbbd03e
NatashaB-randao <natashabrandao57@hotmail.com>
2023-07-25 12:52:36 -0300

    Decodificar dados JSON em dados SwiftUI

--- WeatherApp/WeatherApp/Managers/WeatherManager.swift
+++ WeatherApp/WeatherApp/Managers/WeatherManager.swift
@@ -11,7 +11,56 @@ import Foundation
 import CoreLocation
 
 class WeatherManager {
+    // Solicitação HTTP para obter o clima atual dependendo das coordenadas que obtivemos do LocationManager
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws {
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}") else { fatalError("Missing URL")}
+        // Substitua YOUR_API_KEY no link abaixo pelo seu próprio
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)}&lon=\(longitude)&appid=\("81fdea139f24c00f34a1716d1f4f1286")&units=metric") else { fatalError("Missing URL")}
+        
+        let urlRequest = URLRequest(url: url)
+        
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error fetching weather data")}
     }
+}
+
+// Modelo do corpo da resposta que obtemos ao chamar a API OpenWeather
+struct ResponseBody: Decodable {
+    var coord: CoordinatesResponse
+    var weather: [WeatherResponse]
+    var main: MainResponse
+    var name: String
+    var wind: WindResponse
+
+    struct CoordinatesResponse: Decodable {
+        var lon: Double
+        var lat: Double
+    }
+
+    struct WeatherResponse: Decodable {
+        var id: Double
+        var main: String
+        var description: String
+        var icon: String
+    }
+
+    struct MainResponse: Decodable {
+        var temp: Double
+        var feels_like: Double
+        var temp_min: Double
+        var temp_max: Double
+        var pressure: Double
+        var humidity: Double
+    }
+    
+    struct WindResponse: Decodable {
+        var speed: Double
+        var deg: Double
+    }
+}
+
+extension ResponseBody.MainResponse {
+    var feelsLike: Double { return feels_like }
+    var tempMin: Double { return temp_min }
+    var tempMax: Double { return temp_max }
 }
